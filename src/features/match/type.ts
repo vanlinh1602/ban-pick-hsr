a9d8cdf169e9a8871b0a8b95c896d61fd62b5158
Nguyễn Văn Linh <linhnv1622@gmail.com>
2024-09-18 08:35:59 +0700

    update settings vscode

--- src/features/match/type.ts
+++ src/features/match/type.ts
@@ -1,34 +1,34 @@
-export interface Character {
-    entry_page_id: string;
-    name: string;
-    icon_url: string;
-    display_field: DisplayField;
-    filter_values: FilterValues;
-    desc: string;
-}
+export type Character = {
+  entry_page_id: string;
+  name: string;
+  icon_url: string;
+  display_field: DisplayField;
+  filter_values: FilterValues;
+  desc: string;
+};
 
-interface DisplayField {
-    attr_level_1?: string;
-    attr_level_80?: string;
-}
+type DisplayField = {
+  attr_level_1?: string;
+  attr_level_80?: string;
+};
 
-interface FilterValues {
-    character_rarity: CharacterCombatTypeClass;
-    character_factions?: CharacterCombatTypeClass;
-    character_combat_type: CharacterCombatTypeClass;
-    character_paths: CharacterCombatTypeClass;
-}
+type FilterValues = {
+  character_rarity: CharacterCombatTypeClass;
+  character_factions?: CharacterCombatTypeClass;
+  character_combat_type: CharacterCombatTypeClass;
+  character_paths: CharacterCombatTypeClass;
+};
 
-interface CharacterCombatTypeClass {
-    values: string[];
-    value_types: ValueType[];
-    key: null;
-}
+type CharacterCombatTypeClass = {
+  values: string[];
+  value_types: ValueType[];
+  key: null;
+};
 
-interface ValueType {
-    id: string;
-    value: string;
-    mi18n_key: string;
-    icon: string;
-    enum_string: string;
-}
+type ValueType = {
+  id: string;
+  value: string;
+  mi18n_key: string;
+  icon: string;
+  enum_string: string;
+};
