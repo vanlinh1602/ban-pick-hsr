8697e30658e5367d1f7d0bffea8b2f8378f0a732
Nguyễn Văn Linh <linhnv1622@gmail.com>
2024-09-25 02:09:29 +0700

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,4 @@
-export const BACKEND = 'http://localhost:5053';
+export const BACKEND =
+  import.meta.env.REACT_APP_STAGE === 'development'
+    ? 'http://localhost:5053'
+    : 'https://be-tournament.kuma.id.vn/';
