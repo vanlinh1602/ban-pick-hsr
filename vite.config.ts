e88ff1fd18a550f603f98a11aa8384bcc60d018e
Nguyễn Văn Linh <linhnv1622@gmail.com>
2024-09-30 08:25:37 +0700

    update build script

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true,
-  },
   define: {
     'import.meta.env.REACT_APP_STAGE': JSON.stringify(
       process.env.REACT_APP_STAGE,
