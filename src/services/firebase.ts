9e158b07335e9de32a907c8d08624b51314a6d0e
Nguyễn Văn Linh <linhnv1622@gmail.com>
2024-10-05 20:56:02 +0700

    remove config

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,16 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAkL6HSOyvLmkfiiWVyYUFYSJ2e_4oiRVg',
-  authDomain: 'tournament-c8127.firebaseapp.com',
-  projectId: 'tournament-c8127',
-  storageBucket: 'tournament-c8127.appspot.com',
-  messagingSenderId: '200422573385',
-  appId: '1:200422573385:web:af49aa84b1eae55bb5c698',
-  measurementId: 'G-YPFR4JGMK5',
-};
-
+import firebaseConfig from '../configs/firebase.json';
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
